dafbd30771dd95998effc2dfd8ef38a2480ad77d
Mohit Sarkar <mohitsarkar256@gmail.com>
2023-11-04 09:46:08 +0530

    Hello

--- NewClass.java
+++ NewClass.java
@@ -1,5 +1,23 @@
+class Employee{
+    // Attributes (Adjective)
+    int id;
+    String name;
+
+}
+
+
+
+
+
+//This is main class
 public class NewClass {
     public static void main(String args[]){
-        
+        Employee Mohit= new Employee();
+        Mohit.id= 112;
+        Mohit.name= "Mohit Sarkar";
+
+        System.out.println(Mohit.id);
+        System.out.println(Mohit.name);
+
     }
 }
